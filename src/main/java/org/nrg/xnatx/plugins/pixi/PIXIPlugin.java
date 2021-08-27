9d99a1454cb367e5090cf068aa7eb442d022e801
Andy Lassiter <andrewl@wustl.edu>
2021-08-27 12:53:53 -0500

    Moves everything but preclinical subject demographics to develop branch

--- src/main/java/org/nrg/xnatx/plugins/pixi/PIXIPlugin.java
+++ src/main/java/org/nrg/xnatx/plugins/pixi/PIXIPlugin.java
@@ -3,12 +3,8 @@ package org.nrg.xnatx.plugins.pixi;
 import lombok.extern.slf4j.Slf4j;
 import org.nrg.config.exceptions.ConfigServiceException;
 import org.nrg.config.services.ConfigService;
-import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
-import org.nrg.xdat.om.PixiModelcreation;
-import org.nrg.xdat.om.PixiTreatment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -19,20 +15,7 @@ import java.util.stream.Collectors;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @XnatPlugin(value = "PIXIPlugin", name = "PIXI Plugin",
-            logConfigurationFile = "pixi-logback.xml",
-            entityPackages = "org.nrg.xnatx.plugins.pixi.entities",
-            dataModels = {@XnatDataModel(value = PixiModelcreation.SCHEMA_ELEMENT_NAME,
-                                         singular = "Model Creation",
-                                         plural = "Model Creations",
-                                         code = "MC"),
-                          @XnatDataModel(value = PixiTreatment.SCHEMA_ELEMENT_NAME,
-                                         singular = "Treatment",
-                                         plural = "Treatments",
-                                         code = "T")})
-@ComponentScan({"org.nrg.xnatx.plugins.pixi.entities",
-                "org.nrg.xnatx.plugins.pixi.repositories",
-                "org.nrg.xnatx.plugins.pixi.services.impl",
-                "org.nrg.xnatx.plugins.pixi.rest"})
+            logConfigurationFile = "pixi-logback.xml")
 @Slf4j
 public class PIXIPlugin {
 
@@ -41,8 +24,6 @@ public class PIXIPlugin {
     static {
         jsonFormFiles = new HashMap<>();
         jsonFormFiles.put("xnat:subjectData", "/forms/pixi/preclinical-subject.json");
-        jsonFormFiles.put("xhbm:pixi:animalModel", "/forms/pixi/animal-model.json");
-        jsonFormFiles.put("xhbm:pixi:pdx", "/forms/pixi/pdx.json");
     }
 
     @Autowired
