a4fdd2ce2092fec1f8a5eab08f360f5929812bc1
Mohana Ramaratnam <mohana@xnatworks.io>
2025-07-03 11:45:41 +0530

    PIXI-217: Data Manager for Models

--- src/main/java/org/nrg/xnatx/plugins/pixi/PixiUtils.java
+++ src/main/java/org/nrg/xnatx/plugins/pixi/PixiUtils.java
@@ -4,4 +4,5 @@ public class PixiUtils {
 
     public static final String BLI_SESSION_MODALITY = "BLI";
     public static final String BLI_SCAN_MODALITY = "BLI";
+    public static final String MODLE_DATA_MANAGER_ROLE = "PixiDataManager";
 }
